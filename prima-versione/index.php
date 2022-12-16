ee68625de0c31bad7831a0111a713d25158378ac
Kenta Bilotta <kentabilotta20@gmail.com>
2022-12-16 16:45:12 +0100

    first commit

---
+++ prima-versione/index.php
@@ -0,0 +1,28 @@
+<?php
+    include_once "api-dischi.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <img src="" alt="">
+            <h3>Titolo</h3>
+            <p>author</p>
+            <p>year</p>
+        </div>
+
+    </div>
+    
+</body>
+</html>
